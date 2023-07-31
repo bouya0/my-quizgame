71f5a91113d3840d7e83857765f73aa08784724b
shota <shota19830706@gmail.com>
2023-08-01 04:58:54 +0900

    4つの選択肢すべてを正誤判定できるようにしました

--- app.js
+++ app.js
@@ -10,15 +10,36 @@ const correct = "醤油ラーメン";
 
 document.getElementById("js-question").textContent = question;
 
-const button = document.getElementsByTagName("button");
+const $button = document.getElementsByTagName("button");
 
-button[0].textContent = answers[0];
-button[1].textContent = answers[1];
-button[2].textContent = answers[2];
-button[3].textContent = answers[3];
+$button[0].textContent = answers[0];
+$button[1].textContent = answers[1];
+$button[2].textContent = answers[2];
+$button[3].textContent = answers[3];
 
-button[0].addEventListener("click", () => {
+$button[0].addEventListener("click", () => {
   if (correct === document.getElementsByTagName("button")[0].textContent) {
+    window.alert("正解！");
+  } else {
+    window.alert("不正解！");
+  }
+});
+$button[1].addEventListener("click", () => {
+  if (correct === document.getElementsByTagName("button")[1].textContent) {
+    window.alert("正解！");
+  } else {
+    window.alert("不正解！");
+  }
+});
+$button[2].addEventListener("click", () => {
+  if (correct === document.getElementsByTagName("button")[2].textContent) {
+    window.alert("正解！");
+  } else {
+    window.alert("不正解！");
+  }
+});
+$button[3].addEventListener("click", () => {
+  if (correct === document.getElementsByTagName("button")[3].textContent) {
     window.alert("正解！");
   } else {
     window.alert("不正解！");
