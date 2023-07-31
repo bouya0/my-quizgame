848c73fc2b0a7a7846b9fa14546a57907a7df08c
shota <shota19830706@gmail.com>
2023-07-31 22:02:55 +0900

    問題と選択肢を追加しました

---
+++ app.js
@@ -0,0 +1,17 @@
+const question =
+  "LINEが実施した「ラーメンに関する調査」の結果、最も人気のあったラーメンの種類は次のうちどれでしょうか？";
+const answers = [
+  "醤油ラーメン",
+  "味噌ラーメン",
+  "塩ラーメン",
+  "とんこつラーメン",
+];
+const correct = "醤油ラーメン";
+
+document.getElementById("js-question").textContent = question;
+
+document.getElementsByTagName("button")[0].textContent = answers[0];
+document.getElementsByTagName("button")[1].textContent = answers[1];
+document.getElementsByTagName("button")[2].textContent = answers[2];
+document.getElementsByTagName("button")[3].textContent = answers[3];
+
