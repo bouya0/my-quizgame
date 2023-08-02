7cd3cd49634a039f8628ced0b68090c9aa66c687
shota <shota19830706@gmail.com>
2023-08-02 17:51:45 +0900

    クイズゲームが遊べるようになりました

--- app.js
+++ app.js
@@ -23,40 +23,44 @@ const quiz = [
       "浅草の「六厘舎」",
       "池袋の「一蘭」",
     ],
-    correct: "大勝軒",
+    correct: "中野の「大勝軒」",
   },
 ];
 
-const question =
-  "ラーメンに関する調査の結果、最も人気のあったラーメンの種類は次のうちどれ？";
-const answers = [
-  "醤油ラーメン",
-  "味噌ラーメン",
-  "塩ラーメン",
-  "とんこつラーメン",
-];
-const correct = "醤油ラーメン";
-
-document.getElementById("js-question").textContent = question;
+const quizLength = quiz.length;
+let quizIndex = 0;
 
 const $button = document.getElementsByTagName("button");
 const buttonLength = $button.length;
 
-//クイズの問題
+//クイズの問題文、選択肢を定義
 const setupQuiz = () => {
+  //quiz[インデックス].キー名（連想配列なので）で呼び出す。
+  document.getElementById("js-question").textContent = quiz[quizIndex].question;
   let buttonIndex = 0;
   while (buttonIndex < buttonLength) {
-    $button[buttonIndex].textContent = answers[buttonIndex];
+    $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
     buttonIndex++;
   }
 };
 setupQuiz();
 
+//クリックした時の動きを関数にまとめる
 const clickHandler = (e) => {
-  if (correct === e.target.textContent) {
+  if (quiz[quizIndex].correct === e.target.textContent) {
     window.alert("正解！");
   } else {
     window.alert("不正解！");
+  }
+
+  quizIndex++;
+
+  if (quizIndex < quizLength) {
+    //問題数があればこちらを実行
+    setupQuiz();
+  } else {
+    //問題数がもうなければこちらを実行
+    window.alert("終了！");
   }
 };
 
